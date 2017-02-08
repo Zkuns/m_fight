9ba566f0fef49fab778d22627f2786a61e11f1a9
Z Kun <zhukun6150909@outlook.com>
2017-02-08 13:08:29 +0800

    Test travis

--- spec/models/fight_record_spec.rb
+++ spec/models/fight_record_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe FightRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'travis-ci' do
+    it 'should be true' do
+      expect(true).to be true
+    end
+  end
 end
